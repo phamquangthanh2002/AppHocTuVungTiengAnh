7a0f54ff80447d1c6dfac531bf847000806fabb6
sibud <sibudinss123@gmail.com>
2023-10-20 01:51:10 +0700

    z

---
+++ app/src/main/java/com/example/myquizz/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myquizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button buttonStartQuiz = findViewById(R.id.button_start_quiz);
+        buttonStartQuiz.setOnClickListener(new View.OnClickListener(){
+@Override
+            public void onClick(View v){
+    startQuiz();
+}
+        });
+    }
+    private void startQuiz(){
+        Intent intent = new Intent(MainActivity.this, QuizzActivity.class );
+startActivity(intent);
+    }
+}
