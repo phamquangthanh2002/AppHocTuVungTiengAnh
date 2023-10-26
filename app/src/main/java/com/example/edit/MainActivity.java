2cbc74db2d95751e65bd237ff34e966e4799b990
Admin <thanhquang6913@gmail.com>
2023-10-27 01:22:49 +0700

    thanh

--- app/src/main/java/com/example/edit/MainActivity.java
+++ app/src/main/java/com/example/edit/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.edit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnA;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnA = (Button) findViewById(R.id.button);
+        btnA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,dangnhap.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/edit/dangnhap.java
@@ -0,0 +1,11 @@
+package com.example.edit;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+public class dangnhap extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dangnhap);
+    }
+}
