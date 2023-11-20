bde3c51557f66dccdf4c0ba8e8885dfab72eb821
Admin <thanhquang6913@gmail.com>
2023-11-20 22:53:17 +0700

    thanh

--- app/src/main/java/com/example/edit/MainActivity.java
+++ app/src/main/java/com/example/edit/MainActivity.java
@@ -17,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
         btnA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, tguess.class);
+                Intent intent = new Intent(MainActivity.this, dangnhap.class);
                 startActivity(intent);
             }
         });
