bdf66a9607bdd110e3c0daf20643a2e72f2df183
sibud <sibudinss123@gmail.com>
2023-12-19 22:03:30 +0700

    câu hỏi

--- app/src/main/java/com/example/edit/DashboardActivity.java
+++ app/src/main/java/com/example/edit/DashboardActivity.java
@@ -1,36 +1,195 @@
 package com.example.edit;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.app.Dialog;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class DashboardActivity extends AppCompatActivity {
-CountDownTimer countdowntimer;
+    public static ArrayList<Model> list;
+    int index = 0;
+    Button buttonpink;
+    CountDownTimer countdowntimer;
 
-ProgressBar progressbar;
-private static final long Intervall = 1000;
-private static final long Countdown_time =20000;
+    ProgressBar progressbar;
+    TextView card_question, optiona, optionb, optionc, optiond;
+    CardView cardOA, cardOB, cardOC, cardOD, cardOE;
+    private static final long Intervall = 1000;
+    private static final long Countdown_time = 20000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        cardOE = findViewById(R.id.next);
+
+        list = new ArrayList<>();
+        Collections.shuffle(list);
+        Model question1 = new Model("Câu hỏi 1", "Lựa chọn A", "Lựa chọn B", "Lựa chọn C", "Lựa chọn D", "Lựa chọn C");
+        list.add(question1);
+        Model question2 = new Model("Câu hỏi 2", "Lựa chọn A", "Lựa chọn B", "Lựa chọn C", "Lựa chọn D", "Lựa chọn D");
+        list.add(question2);
+
+        setAllData();
+        next();
         progressbar = findViewById(R.id.quiz_timer);
+
 
         countdowntimer = new CountDownTimer(Countdown_time, Intervall) {
             @Override
             public void onTick(long millisUntilFinished) {
-int timerValue = (int) (millisUntilFinished / 1000);
-progressbar.setProgress(timerValue);
+                int timerValue = (int) (millisUntilFinished / 1000);
+                progressbar.setProgress(timerValue);
             }
 
             @Override
             public void onFinish() {
-Dialog dialog = new Dialog(DashboardActivity.this);
                 progressbar.setProgress(0);
+                Dialog dialog = new Dialog(DashboardActivity.this);
+                dialog.getWindow().addFlags(WindowManager.LayoutParams.FLAG_BLUR_BEHIND);
+                dialog.setContentView(R.layout.time_out_dialog);
+
+                buttonpink = dialog.findViewById(R.id.btn_tryagain);
+
+
+                buttonpink.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(DashboardActivity.this, "Menu Item 1 clicked", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(DashboardActivity.this, DashboardActivity.class);
+                        startActivity(intent);
+                    }
+                });
+                dialog.show();
+
+
             }
-        };countdowntimer.start();
+        };
+        countdowntimer.start();
+
+
+
+    }
+
+
+    private void next(){
+   cardOE.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View view) {
+           resetColor();
+           goToNextQuestion();
+
+       }
+   });
+    }
+    private void setAllData() {
+        card_question = findViewById(R.id.card_question);
+        optiona = findViewById(R.id.card_optiona);
+        optionb = findViewById(R.id.card_optionb);
+        optionc = findViewById(R.id.card_optionc);
+        optiond = findViewById(R.id.card_optiond);
+
+        cardOA = findViewById(R.id.cardA);
+        cardOB = findViewById(R.id.cardB);
+        cardOC = findViewById(R.id.cardC);
+        cardOD = findViewById(R.id.cardD);
+
+
+        if (index < list.size()) {
+            Model question = list.get(index);
+            card_question.setText(question.getQuetion());
+            optiona.setText(question.getOa());
+            optionb.setText(question.getOb());
+            optionc.setText(question.getOc());
+            optiond.setText(question.getOd());
+
+            cardOA.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (question.getAns().equals("Lựa chọn A")) {
+                        cardOA.setCardBackgroundColor(Color.GREEN);
+                    } else {
+                        cardOA.setCardBackgroundColor(Color.RED);
+                    }
+
+                }
+            });
+
+            cardOB.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (question.getAns().equals("Lựa chọn B")) {
+                        cardOB.setCardBackgroundColor(Color.GREEN);
+                    } else {
+                        cardOB.setCardBackgroundColor(Color.RED);
+                    }
+
+                }
+            });
+
+            cardOC.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (question.getAns().equals("Lựa chọn C")) {
+                        cardOC.setCardBackgroundColor(Color.GREEN);
+                    } else {
+                        cardOC.setCardBackgroundColor(Color.RED);
+                    }
+
+                }
+            });
+
+
+            cardOD.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (question.getAns().equals("Lựa chọn D")) {
+                        cardOD.setCardBackgroundColor(Color.GREEN);
+                    } else {
+                        cardOD.setCardBackgroundColor(Color.RED);
+                    }
+
+                }
+
+
+            });
+
+        }
+    }
+
+
+    private void resetColor(){
+        cardOA.setCardBackgroundColor(Color.WHITE);
+        cardOB.setCardBackgroundColor(Color.WHITE);
+        cardOB.setCardBackgroundColor(Color.WHITE);
+        cardOC.setCardBackgroundColor(Color.WHITE);
+    }
+    private void goToNextQuestion() {
+        countdowntimer.cancel();
+        countdowntimer.start();
+
+        index++;
+        if (index < list.size()) {
+
+            setAllData();
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/edit/Model.java
@@ -0,0 +1,62 @@
+package com.example.edit;
+
+public class Model {
+    String Quetion, oa, ob, oc, od ,ans;
+
+    public Model(String quetion, String oa, String ob, String oc, String od, String ans) {
+        Quetion = quetion;
+        this.oa = oa;
+        this.ob = ob;
+        this.oc = oc;
+        this.od = od;
+        this.ans = ans;
+    }
+
+    public String getQuetion() {
+        return Quetion;
+    }
+
+    public void setQuetion(String quetion) {
+        Quetion = quetion;
+    }
+
+    public String getOa() {
+        return oa;
+    }
+
+    public void setOa(String oa) {
+        this.oa = oa;
+    }
+
+    public String getOb() {
+        return ob;
+    }
+
+    public void setOb(String ob) {
+        this.ob = ob;
+    }
+
+    public String getOc() {
+        return oc;
+    }
+
+    public void setOc(String oc) {
+        this.oc = oc;
+    }
+
+    public String getOd() {
+        return od;
+    }
+
+    public void setOd(String od) {
+        this.od = od;
+    }
+
+    public String getAns() {
+        return ans;
+    }
+
+    public void setAns(String ans) {
+        this.ans = ans;
+    }
+}
